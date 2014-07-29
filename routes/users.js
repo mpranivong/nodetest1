ab2c0d3dce5ae5d728dafe06906309b639a37a1f
Michael Pranivong <michael@cfssoftware.com>
2014-07-29 18:33:33 -0500

    Adding user list

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  
+	collection.find({},{},function(e,docs){
+		if (!docs) {
+			res.send('User data empty');
+		} else {
+  		res.render('userlist', {
+  		"userlist" : docs
+  		});
+		}
+	});
+
+});
+
 module.exports = router;
